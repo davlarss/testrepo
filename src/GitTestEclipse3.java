50babc21d64bc1c77fe4d7a88aef42846f96eed1
Mittnamn <testmejjl@mail.com>
2022-12-19 01:49:00 +0100

    lite klargörande kommentarer

--- src/GitTestEclipse3.java
+++ src/GitTestEclipse3.java
@@ -1,4 +1,18 @@
 
+// Nu är det bara att hoppas att Eclipse fungerar väl trots att jag har initierat ett repo inne i Projektkatalogen inne i Workspaces
+// för att få src/ som enda katalog på github när jag pushar dit
+// ett problem verkar eventuellt kunna vara att det blir stökigt om Eclipse söker igenom .git-katalogen,
+// samt att git söker igenom hela Workspace-katalogen.
+// Undrar ifall det är mer lugnt ifall man gör detta och sköter följande git terminalen som jag gjort.
+// Tror ändå att det blir snyggare att ha ett repo per projekt, med bara src/
+// än att ha flera projekt i samma repo... men är väl under utvärdering.
+// 
+// Nu måste jag bara göra samma i Assignment2 (och så småningom övriga med), samt ändra användarnamn och mejl.
+// 
+// Sedan när alla pushar är klara gör det nog inget om jag raderar de lokala repona (eller Eclipse-projekten för den delen heller),
+// då jag alltid kan klona från Github.
+
+
 public class GitTestEclipse3 {
 
 	public static void main(String[] args) {
