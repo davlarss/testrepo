7497ca1aae65fcc6a1d3ed385038066f8bf1035b
Mittnamn <testmejjl@mail.com>
2022-12-19 01:04:50 +0100

    first commit

---
+++ src/GitTestEclipse3.java
@@ -0,0 +1,11 @@
+
+public class GitTestEclipse3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Låt oss testa git");
+		System.out.println("Ändrar lite till");
+	}
+	
+	
+}
